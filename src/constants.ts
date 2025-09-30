f8a4d13aeaf698e907dac14583bce337cb4b9157
afsheenaslam <afsheenaslamit@gmail.com>
2025-09-30 16:11:03 +0500

    solved

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
-export const baseURL = "https://todo-app-backend-bay.vercel.app/";
-// export const baseURL = "http://localhost:3000";
+export const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://todo-app-backend-bay.vercel.app";
